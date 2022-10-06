a6ddfbb7e91950035d16ac81660ab30f0ab3cb02
kapiljundale <jundale.kapil@gmail.com>
2022-10-06 11:44:20 +0530

    First program execuation in JS
    First program execuation in JS

---
+++ 0_Assignment-1.js
@@ -0,0 +1,27 @@
+
+// Printing the full Name
+console.log("Kapil Balasaheb Jundale");
+
+//Any three hobbies
+console.log("Hobbies:");
+console.log("1.Playing Cricket");
+console.log("2.Reading Book");
+console.log("3.Watching Movies");
+
+//Any Three Strength
+console.info("Strength:");
+console.info("1.Hard Working");
+console.info("2.Positive");
+console.info("3.Honest");
+
+//Any Three Weakness
+console.warn("Weakness:");
+console.warn("1.Fear of public speaking");
+console.warn("2.Issues with delegating tasks");
+console.warn("3.Self-criticism");
+
+//Add=30+50
+console.log(30+50);
+
+//multi=49*479
+console.log(49*479);
