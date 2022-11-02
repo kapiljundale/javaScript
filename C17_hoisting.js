a260086b68de391f6f501c177fc467411191600f
kapiljundale <jundale.kapil@gmail.com>
2022-11-02 08:35:14 +0530

    Global Variable, hoisting, local Storage, session Storage
    Global Variable, hoisting, local Storage, session Storage

---
+++ C17_hoisting.js
@@ -0,0 +1,18 @@
+
+//unable to acess but some value is defined
+console.log(myName);
+var myName = "Kapil";
+
+
+//able to acess--because this function is hosted
+show();// call at below as well as above
+function show(){
+    console.log("Show function");
+}
+show();// call at below as well as above
+ 
+
+
+//function expression can not defined early or unabale to acess
+//variable decleared using the var that can be acess or hosted
+// and let and const that can not be acess or hosted.
