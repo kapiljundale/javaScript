49695be9ed214775053c8576cff60bdc1a361786
kapiljundale <jundale.kapil@gmail.com>
2022-11-04 21:47:33 +0530

    local Storage and Array Assignment
    local Storage and Array Assignment

---
+++ 16_Array.js
@@ -0,0 +1,14 @@
+var arrayOfNames = ["kamat","Memon","Nashpati","Taimur","Memon","Kamat","Andy","Taimur"];
+console.log("---------Before removing the duplicat value--------------");
+console.log(arrayOfNames);
+
+console.log("---------After removing the duplicat value--------------");
+var AfterRemovedDuplicate = new Set(arrayOfNames)
+console.log(AfterRemovedDuplicate);
+
+//find out the count of how many duplicat array
+
+
+
+
+//log the total number count unique element available after removing the duplicate
